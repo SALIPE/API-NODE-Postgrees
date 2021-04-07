e63387960b46083a057bc5739c5657f50631e472
Felipe Bueno de Souza <felipebuenosouza@alunos.utfpr.edu.br>
2021-04-06 23:07:49 -0300

    initializing api

---
+++ api/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports={
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'gtstation10',
+    database: 'niceltecDataBase',
+    define: {
+        timestamp: true,
+        underscored: true,
+    },
+
+};
